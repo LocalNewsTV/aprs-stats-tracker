723163e8d7c4ee96cd73eb33fc2b7abca378e69b
Matthew Logan <loganmatthewdev@gmail.com>
2024-03-10 15:47:19 -0700

    create DataFormatter class for graph tools

---
+++ src/utils/DataFormatter.ts
@@ -0,0 +1,29 @@
+import AprsEntryType from "../types/AprsEntryType";
+
+class DataFormatter {
+  static daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',]
+  static timeConversion = [
+    '0:00', '1:00', '2:00', '3:00', '4:00', '5:00',
+    '6:00', '7:00', '8:00', '9:00', '10:00', '11:00',
+    '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
+    '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', 
+  ];
+
+  static DayConversion(dataset: Array<AprsEntryType>) {
+    const entries: Array<number> = Array(7).fill(0);
+    dataset.forEach((item: AprsEntryType) => {
+      entries[new Date(item.time * 1000).getDay()]++
+    })
+    return entries;
+  }
+
+  static HourConversion(dataset: Array<AprsEntryType>) {
+    const entries: Array<number> = Array(24).fill(0);
+    dataset.forEach((item: AprsEntryType) => {
+      entries[new Date(item.time * 1000).getHours()]++
+    })
+    return entries;
+  }
+}
+
+export default DataFormatter;
